f8a6826dd6b76e1824900b619e8a5598bdbc9f5d
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-11-25 01:16:49 +0500

    landing page

---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper_next",
+        prevEl: ".swiper_prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    loop: true,
+
+    autoplay: {
+        delay: 3000,
+    },
+})
+
+// branch swiper
+const swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: ".branch-swiper-next",
+        prevEl: ".branch-swiper-prev",
+    },
+    autoplay: {
+        delay: 3000,
+    },
+
+})
