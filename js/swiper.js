c995d5766c07499dce137ba361ddc82a033b988c
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-11-26 03:12:22 +0500

    add new 2 sections

--- js/swiper.js
+++ js/swiper.js
@@ -26,5 +26,15 @@ const swiper2 = new Swiper(".mySwiper2", {
     autoplay: {
         delay: 3000,
     },
+})
 
+// english foundation swiper
+const foundationswiper = new Swiper(".foundation-swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
 })
